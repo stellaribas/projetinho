076f33813ba4933b4183dcad647b05d74f84f933
Bruno Malheiros <brunopma10@gmail.com>
2019-09-26 14:28:06 -0300

    pacotao de coisas feitass em sala

--- projetinho.py
+++ projetinho.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/cadastro')
+def cadastro():
+    return render_template('_Paginas/Cadastro.html')
+
 if(__name__=='__main__'):
     app.run(debug=True)
