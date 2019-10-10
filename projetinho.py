eeb0e58a94afc7581a09b0c8f305933b08450811
Bruno Malheiros <brunopma10@gmail.com>
2019-10-10 11:54:58 -0300

    rotas das paginas adicionadas

--- projetinho.py
+++ projetinho.py
@@ -10,5 +10,25 @@ def index():
 def cadastro():
     return render_template('_Paginas/Cadastro.html')
 
+@app.route('/Denuncias')
+def Denuncias():
+    return render_template('_Paginas/Denuncias.html')
+
+@app.route('/Login')
+def Login():
+    return render_template('_Paginas/Login.html')
+
+@app.route('/Mapa')
+def Mapa():
+    return render_template('_Paginas/Mapa.html')
+
+@app.route('/QuemSomos')
+def QuemSomos():
+    return render_template('_Paginas/QuemSomos.html')
+
+@app.route('/Curiosidades')
+def Curiosidades():
+    return render_template('_Paginas/Curiosidades.html')
+
 if(__name__=='__main__'):
     app.run(debug=True)
